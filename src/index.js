2d646dd523b01cfa5d43b74c2daa6faffd7b6c55
godcl1623 <delphi5281@gmail.com>
2021-06-23 22:58:16 +0900

    feature - 인풋 + 검증 기능 원하는 방향으로 구현 성공

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 // components
 import App from './components/App';
+import ContactPrac from './components/layouts/Contact_prac'
 // reducer
 import reducers from './reducers';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
+  // <Provider store={createStore(reducers)}>
+  //   <App />
+  // </Provider>,
+  <ContactPrac />,
   document.querySelector('#root')
 );
