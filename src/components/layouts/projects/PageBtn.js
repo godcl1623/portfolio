8522f7b4bf674dc38bd9abd59a919dad8566a388
godcl1623 <delphi5281@gmail.com>
2021-06-26 21:50:00 +0900

    features - Projects - 이전/다음 페이지 기능 구현

--- src/components/layouts/projects/PageBtn.js
+++ src/components/layouts/projects/PageBtn.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectedProject } from '../../../actions';
 
 const PageBtn = ({ direction }) => {
+  const current = useSelector(state => state.selectedProject);
+  const dispatch = useDispatch();
+  const testFunc = btnText => {
+    const projectText = current.split(' ')[0];
+    let projectNumber = Number(current.split(' ')[1]);
+    if (btnText === '▶') {
+      projectNumber += 1;
+    } else {
+      projectNumber -= 1;
+    }
+    // 임시 로직 - 데이터 목록을 배열로 받아서 하드코딩 말고 배열 api로 구현하기
+    if (projectNumber <= 0) {
+      projectNumber = 3;
+    } else if (projectNumber >= 4) {
+      projectNumber = 1;
+    }
+    const test = [projectText, projectNumber].join(' ');
+    console.log(test);
+    dispatch(selectedProject(test));
+  };
+  console.log(current.split(' '));
   const btnText = direction === 'left' ? '◀' : '▶';
+
   return (
     <div className={`container ${direction}`} style={{ border: '1px solid black' }}>
-      <span className={`btn ${direction}`} style={{ border: '1px solid black', width: '50px', height: '50px'}}>{ btnText }</span>
+      <button
+        className={`btn ${direction}`}
+        style={{ border: '1px solid black', width: '50px', height: '50px'}}
+        onClick={() => testFunc(btnText)}
+      >{ btnText }</button>
     </div>
   );
 };
