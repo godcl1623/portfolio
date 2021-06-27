2d18d58fea1db8c3d7d0dc6c4df978ce6c890613
godcl1623 <delphi5281@gmail.com>
2021-06-27 23:09:17 +0900

    develop - emotion.js 설치, App(글로벌), Main 스타일링 진행중

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 // components
 import Modal from './Modal';
 import Contact from './layouts/Contact';
 // action creator
 import { modalHandler } from '../actions';
+// modules
+import { flex, sizes } from '../styles/presets';
 
 /* Component Body */
 const Main = () => {
@@ -17,19 +21,46 @@ const Main = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="Main">
-      <h2>이치행</h2>
-      <h4>프론트엔드 포트폴리오</h4>
-      <hr />
+    <div
+      className="Main"
+      css={css`
+        ${flex.vertical};
+        ${sizes.full}
+        position: relative;
+
+        * {
+          // 임시
+          margin: 20px;
+        }
+      `}
+    >
+      <div
+        className="header-container"
+        css={css`
+          ${flex.vertical};
+          width: 300px;
+          height: 200px;
+          background-color: black;
+          color: white;
+        `}
+      >
+        <h1>이치행</h1>
+        <h2>프론트엔드 포트폴리오</h2>
+      </div>
+      <hr
+        css={css`
+          width: 25%;
+        `}
+      />
       <p>
         프론트엔드 개발자를 희망하는 이치행의 포트폴리오 사이트입니다.
       </p>
       <div className="menu">
-          <button onClick={() => dispatch(modalHandler(true))}>CONTACT</button>
           <Link to="/about">ABOUT</Link>
           <Link to="/works">WORKS</Link>
           <a href="https://github.com/godcl1623" target="_blank" rel="noreferrer noopener">GITHUB</a>
           <a href="https://godcl1623.tistory.com/" target="_blank" rel="noreferrer noopener">BLOG</a>
+          <button onClick={() => dispatch(modalHandler(true))}>CONTACT</button>
       </div>
       {/*
         modalState: false면 모달창 닫힌 상태
---
+++ src/styles/presets.js
@@ -0,0 +1,15 @@
+export const flex = {
+  vertical: `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `
+}
+
+export const sizes = {
+  full: `
+    width: 100%;
+    height: 100%;
+  `
+}
