75d3f7f70207bb09892c29cbcb06bd81d1e09306
godcl1623 <delphi5281@gmail.com>
2021-06-30 01:23:20 +0900

    develop - 전체 컴포넌트 초안 작성 - 디자인 수정 필요

--- src/components/layouts/form/FormLayout.js
+++ src/components/layouts/form/FormLayout.js
@@ -2,6 +2,9 @@
 // Libraries
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { flex } from '../../../styles/presets';
 import FormErrorMsg from './FormErrorMsg';
 
 // hookForm, refs: FormLogic 참조
@@ -30,15 +33,51 @@ const FormLayout = ({ hookForm, refs }) => {
 
   return (
     <div className="form-layout">
-      <form onSubmit={handleSubmit(onSuccess, onErrors)}>
-        <div>
-          <input type="text" placeholder="Name" {...registerInput('name')} />
-          <FormErrorMsg type="name" errorMsg={errorMsgGenerator} />
-          <input type="text" placeholder="Email" {...registerInput('email')} />
-          <FormErrorMsg type="email" errorMsg={errorMsgGenerator} />
+      <form
+        onSubmit={handleSubmit(onSuccess, onErrors)}
+        css={css`
+          ${flex.vertical}
+        `}
+      >
+        <div
+          className="who-are-you"
+          css={css`
+            ${flex.horizontal.center}
+            width: 100%;
+            input {
+              font-size: 16px;
+            }
+            .name {
+              margin-right: 10px;
+            }
+          `}
+        >
+          {/* 에러 메세지 나타나면 input이 위로 밀리는 문제 해결 필요 */}
+          <div className="name">
+            <input type="text" placeholder="Name" {...registerInput('name')}/>
+            <FormErrorMsg type="name" errorMsg={errorMsgGenerator} />
+          </div>
+          <div className="email">
+            <input type="text" placeholder="Email" {...registerInput('email')} />
+            <FormErrorMsg type="email" errorMsg={errorMsgGenerator} />
+          </div>
         </div>
-        <textarea placeholder="Contents" {...registerInput('contents')} />
-        <FormErrorMsg type="contents" errorMsg={errorMsgGenerator} />
+        <div
+          className="tell-your-story"
+          css={css`
+            margin: 20px 10px;
+            width: 100%;
+            textarea {
+              width: 100%;
+              height: 100px;
+              font-size: 16px;
+              overflow-y: scroll;
+            }
+          `}
+        >
+          <textarea placeholder="Contents" {...registerInput('contents')} />
+          <FormErrorMsg type="contents" errorMsg={errorMsgGenerator} />
+        </div>
         <input type="submit" />
       </form>
     </div>
