45ec852878c96db8180f64238a2f016a740dcd9f
godcl1623 <delphi5281@gmail.com>
2021-07-15 02:06:07 +0900

    develop - Projects 터치 이벤트 구현중: 왼쪽, 오른쪽 이벤트 자체는 구현했는데 캐러셀 초기화에서 오작동 발생중(조건에 맞지 않아도 반복 호출됨), 각도 설정 필요

--- src/components/modal/projects/Projects.js
+++ src/components/modal/projects/Projects.js
@@ -5,14 +5,18 @@ import { css } from '@emotion/react';
 import BodySection from './layouts/BodySection';
 import projectsData from '../../../db/projectsData';
 import { slideStartPoint } from '../../../modules/customfunctions';
-import { isReadyToMoveCreator, isChangingProjectCreator } from '../../../actions';
+import { isReadyToMoveCreator, isChangingProjectCreator, selectedProjectCreator } from '../../../actions';
 
 const Projects = () => {
   const modalState = useSelector(state => state.modalState);
-  const changedValue = useSelector(state => state.isChangingProject);
+  const changeState = useSelector(state => state.isChangingProject);
   const selectedProject = useSelector(state => state.selectedProject);
   const readyToMove = useSelector(state => state.isReadyToMove);
+  const list = useSelector(state => state.projectsList);
   const dispatch = useDispatch();
+  const { headers } = projectsData;
+
+  const maxChangeValue = slideStartPoint(headers);
 
   useEffect(() => {
     const makeReady = setTimeout(() => dispatch(isReadyToMoveCreator(false)), 300);
@@ -29,41 +33,120 @@ const Projects = () => {
     });
   }, [selectedProject]);
 
-  // const [startX, setStartX] = React.useState('');
-  // const [endX, setEndX] = React.useState('');
-  // const [startY, setStartY] = React.useState('');
-  // const [endY, setEndY] = React.useState('');
-  // const xVal = startX - endX;
-  // const yVal = startY - endY;
+  // 여기부터 임시
+  const updateNextProjectState = btnText => {
+    const projectText = selectedProject.split(' ')[0];
+    let projectNumber = Number(selectedProject.split(' ')[1]);
+    projectNumber = btnText === '▶' ? projectNumber + 1 : projectNumber - 1;
+    if (projectNumber <= 0) {
+      projectNumber = list.length;
+    } else if (projectNumber > list.length) {
+      projectNumber = 1;
+    }
+    const test = [projectText, projectNumber].join(' ');
+    dispatch(selectedProjectCreator(test));
+  };
 
-  // useEffect(() => {
-  //   const fee = document.querySelector('.Projects');
-  //   fee.addEventListener('touchstart', e => {
-  //     setStartX(e.changedTouches['0'].screenX);
-  //     setStartY(e.changedTouches['0'].screenY);
-  //   }, false);
-  //   fee.addEventListener('touchend', e => {
-  //     setEndX(e.changedTouches['0'].screenX);
-  //     setEndY(e.changedTouches['0'].screenY);
-  //   }, false);
-  //   console.log(xVal, yVal)
-  //   if (xVal > yVal) {
-  //     fee.addEventListener('touchmove', e => {
-  //       e.preventDefault();
-  //     }, false);
-  //     if (xVal > 0) {
-  //       dispatch(isChangingProjectCreator(changedValue-100));
-  //     } else {
-  //       dispatch(isChangingProjectCreator(changedValue+100));
-  //     }
-  //   } else {
-  //     fee.addEventListener('touchmove', e => {
-  //       console.log('foo')
-  //     }, false);
-  //   }
-  // }, [xVal, yVal]);
+  const changeActualProject = btnText => {
+    const foo = document.querySelector('.Projects');
+    foo.style.transition = 'all 0.4s';
+    if (btnText === '▶') {
+      if (-changeState === maxChangeValue * 2) {
+        dispatch(isChangingProjectCreator(changeState-100));
+        dispatch(isReadyToMoveCreator(true));
+        setTimeout(() => {
+          foo.style.transition = ''
+          dispatch(isChangingProjectCreator(0));
+        }, 400);
+      } else {
+        dispatch(isChangingProjectCreator(changeState-100));
+      }
+    } else if (btnText === '◀') {
+      if (changeState === 0) {
+        dispatch(isChangingProjectCreator(changeState+100));
+        dispatch(isReadyToMoveCreator(true));
+        setTimeout(() => {
+          foo.style.transition = '';
+          dispatch(isChangingProjectCreator(-maxChangeValue * 2));
+        }, 400);
+      } else {
+        dispatch(isChangingProjectCreator(changeState+100));
+      }
+    }
+  };
+  // 여기까지 임시
 
-  const { headers } = projectsData;
+  const [startX, setStartX] = React.useState('');
+  const [endX, setEndX] = React.useState('');
+  const [startY, setStartY] = React.useState('');
+  const [endY, setEndY] = React.useState('');
+
+  useEffect(() => {
+    const fee = document.querySelector('.Projects');
+    fee.style.transition = 'all 0.4s';
+
+    const dragStart = event => {
+      event.preventDefault();
+      setStartX(event.touches[0].clientX);
+      setStartY(event.touches[0].clientY);
+    }
+
+    const dragAction = event => {
+      setEndX(event.touches[0].clientX);
+      setEndY(event.touches[0].clientY);
+    }
+
+    const dragEnd = event => {
+      const diffX = startX - endX;
+      const diffY = startY - endY;
+      const absDiffX = Math.abs(diffX);
+      const absDiffY = Math.abs(diffY);
+      // console.log(typeof diffX, typeof diffY, typeof absDiffX, typeof absDiffY);
+      // console.log(Math.abs(startX - endX), Math.abs(startY - endY));
+      // console.log(startY - endY);
+      /* ******************** 로직 본체 ******************** */
+      if (Math.abs(startX - endX) > Math.abs(startY - endY)) {
+        // dispatch(isChangingProjectCreator(changeState-100));
+        if (startX - endX > 0) {
+          updateNextProjectState('▶');
+          if (-changeState === maxChangeValue * 2) {
+            dispatch(isChangingProjectCreator(changeState-100));
+            dispatch(isReadyToMoveCreator(true));
+            setTimeout(() => {
+              fee.style.transition = ''
+              dispatch(isChangingProjectCreator(0));
+              console.log('초기화')
+            }, 400);
+          } else {
+            dispatch(isChangingProjectCreator(changeState-100));
+            console.log('이동')
+          }
+          // console.log('오른쪽');
+        } else if (startX - endX < 0) {
+          // if (changeState === 0) {
+          //   dispatch(isChangingProjectCreator(changeState+100));
+          //   dispatch(isReadyToMoveCreator(true));
+          //   setTimeout(() => {
+          //     fee.style.transition = '';
+          //     dispatch(isChangingProjectCreator(-maxChangeValue * 2));
+          //   }, 400);
+          // } else {
+          //   dispatch(isChangingProjectCreator(changeState+100));
+          // }
+        }
+        // console.log('foo');
+      } else if (Math.abs(startX - endX) < Math.abs(startY - endY)) {
+        if (startY - endY > 0) {
+          console.log('아래쪽');
+        } else if (startY - endY < 0) {
+          console.log('위쪽');
+        }
+      }
+    }
+      fee.addEventListener('touchstart', dragStart);
+      fee.addEventListener('touchmove', dragAction);
+      fee.addEventListener('touchend', dragEnd);
+  }, [endX, endY]);
 
   const Bodies = data => {
     const test = [];
@@ -112,7 +195,7 @@ const Projects = () => {
         // transition: ${readyToMove ? '' : 'all 0.4s'};
         // transition: all 0.4s;
         position: relative;
-        left: ${slideStartPoint(headers) + changedValue}%;
+        left: ${slideStartPoint(headers) + changeState}%;
       `}
     >
       { Bodies(projectsData) }
