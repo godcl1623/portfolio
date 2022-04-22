6f47c780006c8461df45430e3f32fd572108ed33
godcl1623 <delphi5281@gmail.com>
2022-04-22 23:31:35 +0900

    portfolio - features - 프로젝트 텍스트 모듈식으로 변경

--- src/db/projectsData.js
+++ src/db/projectsData.js
@@ -1,39 +1,21 @@
-import source1 from '../examples/crd/react_stock.jpg';
-import source2 from '../examples/libmng/libmng_meta_main.png';
-import source3 from '../examples/por/port_main.png';
+import project1 from './resources/project1';
+import project2 from './resources/project2';
+import project3 from './resources/project3';
 
 const projectsData = {
-  headers: ['clone-react-dnd', 'libmanage', 'portfolio'],
+  headers: [project1.header, project2.header, project3.header],
   images: {
-    'clone-react-dnd': [source1],
-    'libmanage': [source2],
-    'portfolio': [source3]
+    'clone-react-dnd': project1.images,
+    'libmanage': project2.images,
+    'portfolio': project3.images
   },
   icons: {
-    'clone-react-dnd': ['Icon 1', 'Icon 2', 'Icon 3'],
-    'libmanage': ['Icon 1'],
-    'portfolio': ['Icon 1', 'Icon 2', 'Icon 3', 'Icon 4']
+    'clone-react-dnd': project1.icons,
+    'libmanage': project2.icons,
+    'portfolio': project3.icons
   },
-  comments: [
-    `목적 및 기능
-      - Library Manager 앱에 드래그 앤 드롭 기능 추가 및 클론 코딩을 통한 React.js 이해도 제고
-      - 연산 수행 및 상태 관리 수행을 통해 사용자의 드래그 앤 드롭 기능 구현의 보조
-    `,
-
-    `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin lorem mi, venenatis vitae ligula eget, suscipit cursus quam. In venenatis tincidunt lectus id dapibus. Vivamus eget orci neque. Maecenas in interdum leo. Integer congue, quam vulputate tristique feugiat, ipsum justo pulvinar lacus, vel maximus lectus dolor ac mauris. In hac habitasse platea dictumst. Morbi odio sapien, maximus ac pharetra et, imperdiet vel ante. Mauris condimentum, risus vitae aliquam finibus, lectus metus feugiat velit, vel accumsan arcu diam nec ligula. Nulla facilisi. Quisque nisi odio, tempus vitae sodales a, scelerisque vel enim. Nam sollicitudin scelerisque felis id sagittis.
-
-    Nam velit neque, facilisis et elit sit amet, rhoncus condimentum diam. Praesent a nisl a dolor eleifend elementum. Curabitur sem leo, consequat quis dapibus sed, ultricies ac nulla. Duis accumsan convallis pulvinar. Nulla ut sagittis massa, vitae vehicula odio. Vivamus euismod elit ligula, vel accumsan nulla ullamcorper ac. Sed tempus sodales felis, nec scelerisque massa scelerisque quis. Aenean iaculis convallis odio ac fringilla. Nulla sit amet erat laoreet nisi hendrerit faucibus eu at lectus. Aenean eget libero sit amet lacus egestas tempus. Proin elementum tempor mi quis interdum. Nam porta ipsum eu tellus mattis blandit. Donec semper lacus tellus. Etiam luctus auctor bibendum. Maecenas finibus mollis urna, a congue lectus ultricies vel. Cras condimentum nisl vel ligula pellentesque, id vestibulum lectus molestie.`,
-
-    `Sed et fringilla tortor, eu feugiat dui. Mauris magna lorem, aliquet id pretium vitae, porttitor et libero. Phasellus feugiat bibendum metus a fermentum. In vel turpis porta, finibus massa sit amet, feugiat mi. Suspendisse fermentum nibh vitae sagittis dignissim. Pellentesque ut metus sollicitudin, lacinia ipsum vel, pellentesque lacus. Duis sit amet pellentesque arcu. Nulla eget mauris neque. Maecenas laoreet fermentum augue ut tincidunt. Integer euismod arcu mauris, ut cursus justo fermentum sed. Curabitur ornare, metus vitae auctor vulputate, ipsum eros molestie orci, eget luctus velit urna at neque. Praesent eget libero fermentum mi accumsan maximus id sit amet metus. Pellentesque fermentum, erat ac vestibulum auctor, orci nisi commodo ex, nec congue nisl nibh id dui. Vivamus ut mauris eros. Nam tempus turpis in elementum feugiat.
-
-    In quis odio volutpat, ultricies purus a, dapibus dolor. Pellentesque id nisl molestie metus luctus elementum. Nam consectetur cursus cursus. Nulla suscipit iaculis lectus, id dictum neque consectetur id. Donec leo quam, suscipit et iaculis eu, mattis ut nisl. Phasellus id massa convallis, ultrices urna at, semper justo. Sed sed eros porta, tempor felis eget, eleifend nisl. Phasellus ipsum sapien, venenatis ut dictum at, euismod id ante. Aliquam pulvinar vitae lectus eget cursus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris sed euismod nulla. Vestibulum consequat enim dolor, quis efficitur ipsum tempor vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-    Mauris feugiat ornare dui quis volutpat. Ut arcu augue, lacinia et volutpat vitae, varius a tellus. Cras fringilla vel orci quis tempor. Praesent a tortor iaculis, tempor purus id, condimentum ligula. Nam tempus a velit tempus volutpat. Morbi iaculis erat elit, ut finibus eros aliquet quis. Suspendisse id pharetra erat. Vivamus nisi mauris, eleifend ac condimentum sed, laoreet non nibh. Quisque leo lorem, dignissim sit amet neque eget, fringilla lobortis odio. Donec dui eros, finibus eu tincidunt eu, vulputate id dolor. Aliquam et finibus eros, non congue turpis. Praesent est quam, finibus vel semper ut, bibendum non ipsum. In dictum sapien non lacus rhoncus, quis finibus nisl vulputate.
-
-    Nullam sollicitudin in velit a facilisis. Sed at eleifend ante. Nunc quis risus elementum, tristique nisi et, porta nisi. Nullam placerat posuere tincidunt. Vestibulum eu est nec magna aliquet aliquet vel sit amet ex. Suspendisse non laoreet tortor. Nulla pellentesque lacus nec sagittis rutrum. In maximus sapien magna, sed dictum lectus volutpat eget. Nullam quis sapien nec dolor fringilla aliquam. Quisque viverra non elit vel semper. Curabitur vestibulum velit augue, at luctus libero interdum a. Duis sit amet est sodales, dignissim lectus sit amet, feugiat justo. Nam dignissim nibh eu est mattis tincidunt. Phasellus et tempus dolor.`
-  ],
-
-  preview: [source1, source2, source3]
+  comments: [project1.comment, project2.comment, project3.comment],
+  preview: [project1.images[0], project2.images[0], project3.images[0]]
 }
 
 export default projectsData;
